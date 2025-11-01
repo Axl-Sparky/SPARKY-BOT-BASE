21f2e24425df5bbcda3fc1142e3b3d7dd87b5044
Gwak <130032226+Axl-Sparky@users.noreply.github.com>
2025-11-01 18:28:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:d7907e5fae1d440b95cd563f94bc005a",
-    BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://url.sparky.biz.id/5ftLiA.jpg",
+    BOT_INFO: process.env.BOT_INFO || "WP BOT BASE;IDK;https://url.sparky.biz.id/5ftLiA.jpg",
     DATABASE_URL: "postgresql://okkkkkkkkkk_user:4084dqakxjzVc11ghEM0FHc6YhvoOpLt@dpg-d41rerbe5dus73f6jmm0-a.oregon-postgres.render.com/okkkkkkkkkk",
     PREFIX: process.env.PREFIX || "^",
     MODE: "public",
-    SUDO: "917012984396",
+    SUDO: "919539412641",
 }
+
